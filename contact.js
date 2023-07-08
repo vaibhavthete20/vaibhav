a599cf8997ec4fac9c66bf065a0278389210d7e6
vaibhav <vaibhav@gmail.com>
2023-07-09 00:58:25 +0530

    git commit confliction inthe newbranch

--- contact.js
+++ contact.js
@@ -1,4 +1,4 @@
-console.log('in anther branch called contact');
-console.log('in anther branch called contact');
-console.log('in anther branch called contact');
-console.log('in anther branch called contact');
+console.log('in anther branch called contact222');
+console.log('in anther branch called contact222');
+console.log('in anther branch called contact222');
+console.log('in anther branch called contact222');
